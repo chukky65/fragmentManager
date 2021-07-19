3589d58d01f146c07ab7d3b6062d7e0c9cd5639a
chukky65 <ogachuchu65@gmail.com>
2021-07-19 13:41:52 +0100

    Implement fragment manager

--- app/src/main/java/com/chuks/fragmentmanager/fragment/HomeFragment.kt
+++ app/src/main/java/com/chuks/fragmentmanager/fragment/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.chuks.fragmentmanager.R
 import com.chuks.fragmentmanager.databinding.ActivityMainBinding
 import com.chuks.fragmentmanager.databinding.FragmentHomeBinding
@@ -12,6 +15,7 @@ import com.chuks.fragmentmanager.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 private lateinit var binding: FragmentHomeBinding
+ private lateinit var manager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,8 +28,27 @@ private lateinit var binding: FragmentHomeBinding
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentHomeBinding.inflate(inflater, container, false)
+        manager = requireActivity().supportFragmentManager
+        binding.apply {
+            nextPage.setOnClickListener{
+                openNextFragmentkt()
+            }
+        }
         return binding.root
     }
+private fun openNextFragmentkt(){
+    manager.commit {
+        replace<NewFragment>(R.id.fragmentContainerView)
+        setReorderingAllowed(true)
+        addToBackStack("")
+    }
 
+}
+    private fun openNextFragmentJava(){
+        manager.beginTransaction().replace(R.id.fragmentContainerView, NewFragment::class.java, null)
+            .setReorderingAllowed(true)
+            .addToBackStack("")
+
+    }
    
 }
